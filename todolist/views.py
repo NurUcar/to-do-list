d66f83064b74789b911293050b4067fcda9ba13c
Nur Uçar <nur0ucar@gmail.com>
2021-04-12 20:22:40 +0300

    init

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def register(request):
+    pass
+
+
+def login(request):
+    pass
+
+
+def logout(request):
+    pass
+
+
+def todolist(request):
+    pass
