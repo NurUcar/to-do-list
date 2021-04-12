6fbb0b40e16e34c1b8d96125b33c005de103a471
Nur Uçar <nur0ucar@gmail.com>
2021-04-12 22:58:30 +0300

    add login function

--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, auth
 from django.contrib import messages
 
 
@@ -15,16 +15,23 @@ def register(request):
                 request, 'İsim-Soyisim alanı doldurulması gereken bir alandır.')
         elif len(password1) < 6:
             messages.info(request, 'Şifreniz en az 6 karakterden oluşmalıdır.')
-        elif User.objects.filter(email=email):
-            messages.error(request, 'Email adresiniz daha önce başka bir kullanıcı tarafından kullanılmıştır.Eğer şifrenizi unuttuysanız "Giriş Yap" kısmında yer alan "Şifremi Unuttum" seçeneğini kullanabilirsiniz.Bir terslik olduğunu düşünüyorsanız lütfen bizimle iletişime geçiniz.')
         elif len(first_name) < 5:
             messages.info(
                 request, 'İsim Soyisim en az 5 karakterden oluşmalıdır.')
+        elif User.objects.filter(email=email):
+            messages.error(request, 'Email adresiniz daha önce başka bir kullanıcı tarafından kullanılmıştır.Eğer şifrenizi unuttuysanız "Giriş Yap" kısmında yer alan "Şifremi Unuttum" seçeneğini kullanabilirsiniz.')
+
         else:
             if password1 == password2:
-                pass
+                user = User.objects.create_user(
+                    first_name=full_name, email=email, password=password1)
+                user.save()
+                return render(request, 'todolist/login.html')
+            else:
+                messages.info(request, 'Şifreleriniz birbiri ile eşleşmiyor.')
+               
     else:
-        pass
+        return render(request, 'todolist/register.html')
 
 
 def login(request):
