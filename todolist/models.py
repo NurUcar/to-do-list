d842699d8f28239579a18efeeaa4a2b32d4255a7
Nur Uçar <nur0ucar@gmail.com>
2021-04-14 05:39:27 +0300

    add toDoLists and toDoListItem Models

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class toDoLists(models.Model):
+    user = models.ForeignKey(
+        User, related_name="user", on_delete=models.CASCADE)
+    list_name = models.TextField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class toDoListItem(models.Model):
+    STATUS = (
+        ('Complete', 'Complete'),
+        ('On Progress', 'On Progress'),
+        ('Expired', 'Expired'),
+    )
+    toDoList = models.ForeignKey(
+        toDoLists, related_name="toDoList", on_delete=models.CASCADE)
+    item_name = models.TextField(null=False, blank=False)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=15, choices=STATUS, default='On Progress')
+    dead_line = models.DateTimeField()
