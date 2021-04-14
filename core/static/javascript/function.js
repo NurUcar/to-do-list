f57fed913123abf928ca65a64df574375098604e
Nur Uçar <nur0ucar@gmail.com>
2021-04-15 01:41:58 +0300

    add item search, order, filter front-end

--- core/static/javascript/function.js
+++ core/static/javascript/function.js
@@ -1,7 +1,11 @@
-
 function toggle() {
     var blur = document.getElementById('blur');
     blur.classList.toggle('active')
     var popup = document.getElementById('pop-up');
     popup.classList.toggle('active')
 }
+
+/*
+	Dropdown for filter
+*/
+
